42615ab839c55c4b32d90b919939447b7c99120a
Chris Chan <chrisstudio1016@gmail.com>
2025-03-24 21:30:24 +0800

    Fix: main.go server listen

--- server/UserService/main.go
+++ server/UserService/main.go
@@ -1,2 +1,16 @@
 package main
 
+import (
+	"Medium-UserService/internal"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	internal.InitMongoDB()
+	app := fiber.New()
+
+	internal.Router(app)
+
+	app.Listen("3000")
+}
